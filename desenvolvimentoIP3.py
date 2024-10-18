e05449fecf74047748ed78d1810ea23ad1ab97f6
David Mizrahy Neto <160736321+DavidMzNeto@users.noreply.github.com>
2024-10-18 10:19:37 -0300

    Add files via upload

---
+++ desenvolvimentoIP3.py
@@ -0,0 +1,18 @@
+andar = 20
+while True:
+    if andar != 13:
+        print(andar)
+    andar -= 1
+    if andar == 0:
+        break
+
+    for andar in range(20, 0, -1):
+    if andar == 13:
+        continue
+    print(andar)
+
+    andar = 20
+while andar > 0:
+    if andar != 13:
+        print(andar)
+    andar -= 1
